053d5cb72210fe9e7cf426855817dc355f0cf595
Samuel <samutavares44@gmail.com>
2024-04-22 21:19:02 -0300

    feat: Adicionando filtro na página de indicados & página Contatos na navbar.

--- negocios/views.py
+++ negocios/views.py
@@ -11,7 +11,16 @@ class ListaNegociosViews(ListView):
     paginas = 9
 
     def get_queryset(self):
-        return Negocio.objects.all()
+        queryset = Negocio.objects.all()
+        categoria = self.request.GET.get('categoria')
+        tipo_negocio = self.request.GET.get('tipo_negocio')
+
+        if categoria:
+            queryset = queryset.filter(categoria=categoria)
+        if tipo_negocio:
+            queryset = queryset.filter(tipo_negocio=tipo_negocio)
+
+        return queryset
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -19,5 +28,9 @@ class ListaNegociosViews(ListView):
         paginator = Paginator(negocios, self.paginas)
         page_number = self.request.GET.get('page')
         page_obj = paginator.get_page(page_number)
+        context = super().get_context_data(**kwargs)
+        context['categorias'] = Negocio.CATEGORIA_CHOICES
+        context['tipos_negocio'] = Negocio.TIPO_DE_NEGOCIO_CHOICES
         context['negocios'] = page_obj
+        
         return context
