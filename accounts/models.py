3c4602f079feeaedf1e8789849b708938cef581e
Samuel <samutavares44@gmail.com>
2024-05-16 17:11:19 -0300

    fix: definindo email único para os usuários / mensagem de dados inválidos no login.

--- accounts/models.py
+++ accounts/models.py
@@ -1,10 +1,10 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-
 class CustomUser(AbstractUser):
     pass
     full_name = models.CharField(max_length=150, blank=True, null=True)
+    email = models.EmailField(unique=True)
     # add additional fields in here
 
     def __str__(self):
